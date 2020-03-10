5abf57023c1d7c895481d1fcad557a1999b458e8
RIcardoVinicioJaraJara <49212955+RIcardoVinicioJaraJara@users.noreply.github.com>
2020-03-09 19:34:02 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
+
+
+app = Flask(__name__)
+
+app.secret_key = "mysecretkey"
+
+
+
+@app.route("/")
+def index():
+    return render_template("login.html")
+
+
+@app.route("/celular")
+def celular():
+    return render_template("celular.html")
+
+
+@app.route("/pantalla")
+def pantalla():
+    return render_template("pantalla.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='localhost', port=5001)
